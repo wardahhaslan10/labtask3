79787704c0d25bc08acdad031f75839ee790fdfd
wardahhaslan01 <wardahhaslan01@gmail.com>
2026-09-07 09:48:34 +0800

    push

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ Class: DDT7B
 */
 
 include "db.php";
-
 // Get search keyword from URL
 $search = "";
 
@@ -41,10 +40,8 @@ if ($search != "") {
     $result = $conn->query($sql);
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
